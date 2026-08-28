b07654a8244dc053a132ce07d9a20abca7b97fc7
AMANDA CRISTINA OLIVEIRA MARIANO LUZ <85713467+AmanddaLuz@users.noreply.github.com>
2026-08-28 20:14:12 -0300

    Atualizar o CieloPaymentIntentLauncherImpl.kt

--- app/src/main/java/br/com/amandaluz/cielotickets/payment/cielo/CieloPaymentIntentLauncherImpl.kt
+++ app/src/main/java/br/com/amandaluz/cielotickets/payment/cielo/CieloPaymentIntentLauncherImpl.kt
@@ -3,7 +3,6 @@ package br.com.amandaluz.cielotickets.payment.cielo
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.core.net.toUri
 
 class CieloPaymentIntentLauncherImpl(
